15d8b6104688c4ea65abdc33a483f90b51eeabaf
bakmaks <bakmaks@.com>
2020-04-08 14:57:25 +0300

    Фибо с декоратором

--- Фибоначи/fib.py
+++ Фибоначи/fib.py
@@ -1,7 +1,24 @@
 from rcviz import viz
 from PIL import Image
 
-@viz
+
+def memo(func):
+    """
+    Декоратор для кеширования fib(n)
+    При этом мы избавляемся от ранеее объявленной, глобальной переменной "cache"
+    :param func:
+    :return:
+    """
+    temp = {}
+    def wrap(n):
+        if n not in temp:
+            temp[n] = func(n)
+        return temp[n]
+
+    return wrap
+
+
+# @viz
 def fib1(n: int):
     """
     Простая рекурсивная ф. для вычисления числа Фибоначи.
@@ -9,29 +26,50 @@ def fib1(n: int):
     """
     assert n >= 0
     return n if n <= 1 else fib1(n-1) + fib1(n-2)
-
+#----------------------------------------------------------------------------------------------------
 # Глобальная переменная "cache", что бы при выходе из рекурсии повторно не вычилялись значения n
 cache = {}
 
-@viz
+# @viz
 def fib2(n):
     assert n >= 0
     if n not in cache:
         cache[n] = n if n <= 1 else fib2(n-1) + fib2(n-2)
     return cache[n]
+#------------------------------------------------------------------------------------------------------
 
 
-print(fib1(5))     # 21
-image = Image.open('fib1.png')
-image.show()
+@memo
+def fib(n):
+    """
+    Простая рекурсивная ф. для вычисления числа Фибоначи.
+    Очень медленная работа, но с декоратором
+    """
+    assert n >= 0
+    return n if n <= 1 else fib(n-1) + fib(n-2)
 
 
-print(fib2(5))     # 21
-image1 = Image.open('fib2.png')
-image1.show()
-
-print(fib2(800))
+# print(fib1(8))     # 21
+# # Строки вызываются если раскомментировать декоратор @viz у fib1(n) и n <= 5
+# # иначе очень большой файл изображения
+# # image = Image.open('fib1.png')
+# # image.show()
+#
+#
+# print(fib2(8))     # 21
+# # Строки вызываются если раскомментировать декоратор @viz у fib2(n) и n <= 10
+# # иначе очень большой файл изображения
+# # image1 = Image.open('fib2.png')
+# # image1.show()
+#
+# print(fib2(800))
 
 # Превышение глубины рекурсии
-print(fib2(8000))   # RecursionError: maximum recursion depth exceeded in comparison
+# print(fib2(8000))   # RecursionError: maximum recursion depth exceeded in comparison
+
+
+print('Работа декоратора :')
+print('n = 80')
+f = fib(80)
+print(f)
 
