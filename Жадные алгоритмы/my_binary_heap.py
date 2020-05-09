7c319588d8567c84bbbf6d88a66be4c3e30cd37f
bakmaks <gitbakmax@yandex.ru>
2020-05-09 10:56:13 +0300

    Реализован метод insert(x) класса MyBinaryHeap

--- Жадные алгоритмы/my_binary_heap.py
+++ Жадные алгоритмы/my_binary_heap.py
@@ -11,9 +11,36 @@
 class MyBinaryHeap:
     max_heap = []   # Куча с максимумом в корне.
     heap_size = 0
+    index = 0
 
     def insert(self, x):
-        pass
+        self.max_heap.append(x)
+        self.heap_size = len(self.max_heap)
+        self.index = self.heap_size - 1
+        self.siftup(self.index)
 
     def extract_max(self):
-        pass
+        return self.max_heap[self.index]
+
+    def siftup(self, i: int):
+        while 0 < i < self.heap_size:
+            parent = (i - 1) // 2
+            if self.max_heap[i] > self.max_heap[parent]:
+                self.max_heap[i], self.max_heap[parent] = self.max_heap[parent], self.max_heap[i]
+                i = parent
+            else:
+                break
+
+
+
+if __name__ == '__main__':
+    a = MyBinaryHeap()
+    a.insert(20)
+    a.insert(100)
+    a.insert(200)
+    a.insert(50)
+    a.insert(101)
+    a.insert(65)
+    a.insert(201)
+    a.insert(180)
+    print(a.max_heap)
