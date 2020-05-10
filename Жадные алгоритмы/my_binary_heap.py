666bf744f61849f2323b9a156d0c5399cf3d2fc4
bakmaks <gitbakmax@yandex.ru>
2020-05-10 12:02:17 +0300

    --

--- Жадные алгоритмы/my_binary_heap.py
+++ Жадные алгоритмы/my_binary_heap.py
@@ -12,16 +12,24 @@ class MyBinaryHeap:
     max_heap = []   # Куча с максимумом в корне.
     heap_size = 0
 
+    def get(self):
+        # print(self.max_heap)
+        return self.max_heap
+
     def insert(self, x):
         self.max_heap.append(x)
         self.heap_size = len(self.max_heap)
         self.sift_up(self.heap_size - 1)
 
     def extract_max(self):
-        root = self.max_heap[0]
-        self.max_heap[0] = self.max_heap.pop()
-        self.heap_size = len(self.max_heap)
-        self.sift_down(0)
+        if self.heap_size > 1:
+            root = self.max_heap[0]
+            self.max_heap[0] = self.max_heap.pop()
+            self.heap_size = len(self.max_heap)
+            self.sift_down()
+        else:
+            root = 0 if self.heap_size < 1 else self.max_heap[0]
+        # print(root)
         return root
 
     def sift_up(self, i: int):
@@ -33,39 +41,72 @@ class MyBinaryHeap:
             else:
                 break
 
-    def sift_down(self, i: int):
+    def sift_down(self, i: int = 0):
         while 0 <= i < self.heap_size:
             left_child = 2 * i + 1
             right_child = 2 * i + 2
             if right_child < self.heap_size:
-                if self.max_heap[left_child] < self.max_heap[right_child]:
+                if self.max_heap[left_child] > self.max_heap[right_child]:
                     if self.max_heap[i] < self.max_heap[left_child]:
                         self.max_heap[i], self.max_heap[left_child] = self.max_heap[left_child], self.max_heap[i]
                         i = left_child
+                    else:
+                        break
                 else:
                     if self.max_heap[i] < self.max_heap[right_child]:
                         self.max_heap[i], self.max_heap[right_child] = self.max_heap[right_child], self.max_heap[i]
                         i = right_child
+                    else:
+                        break
             elif left_child < self.heap_size:
                 if self.max_heap[i] < self.max_heap[left_child]:
                     self.max_heap[i], self.max_heap[left_child] = self.max_heap[left_child], self.max_heap[i]
                     i = left_child
+                else:
+                    break
             else:
                 break
 
 
 
 if __name__ == '__main__':
+    import random as rnd
     a = MyBinaryHeap()
-    a.insert(50)
-    a.insert(6)
-    a.insert(100)
-    a.insert(9)
-    a.insert(20)
-    a.insert(200)
-    a.insert(201)
-    a.insert(150)
-    a.insert(45)
-    print(a.max_heap)
-    print(a.extract_max())
-    print(a.max_heap)
+    in_range = int(input('Введите диапазон: '))
+    step = int(input('Введите шаг: '))
+    x = [i for i in range(in_range)]
+    rnd.shuffle(x)
+    i = 0
+    old_i = 0
+    flag = True
+    for el in x:
+        a.insert(el)
+        i += 1
+        if i == old_i + step:
+            max_el = max(a.get())
+            max_ex_el = a.extract_max()
+            if max_el != max_ex_el:
+                flag = False
+                print('i = ', i)
+                print(f'max_el = %s, max_ex_el %s ' % (max_el, max_ex_el))
+                break
+            old_i = i
+
+
+    # a.insert(50)
+    # a.insert(6)
+    # a.insert(100)
+    # a.insert(9)
+    # a.insert(20)
+    # a.insert(200)
+    # a.insert(201)
+    # a.insert(150)
+    # a.insert(45)
+    # a.get()
+    # a.extract_max()
+    # a.get()
+    # a.extract_max()
+    # a.get()
+    # a.insert(250)
+    # a.get()
+
