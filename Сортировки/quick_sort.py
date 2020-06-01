843926bc0618bfef666e16e2fc9e7ed4357961fd
bakmaks <gitbakmax@yandex.ru>
2020-06-01 10:37:31 +0300

    --

--- Сортировки/quick_sort.py
+++ Сортировки/quick_sort.py
@@ -1,13 +1,14 @@
+def partition(a, l, r):
+    x = a[l]
+    j = l
+    for i in range(l + 1, r):
+        if a[i] <= x:
+            j += 1
+            a[j], a[i] = a[i], a[j]
+    a[l], a[j] = a[j], a[l]
+    return j
+
 def quick_sort(A, l=0, r=0):
-    def partition(a, l , r):
-        x = a[l]
-        j = l
-        for i in range(l+1, r):
-            if a[i] <= x:
-                j += 1
-                a[j], a[i] = a[i], a[j]
-        a[l], a[j] = a[j], a[l]
-        return j
 
     r = r if r != 0 else len(A)
     if l >= r:
@@ -17,4 +18,6 @@ def quick_sort(A, l=0, r=0):
     quick_sort(A, m+1, r)
 
 if __name__ == '__main__':
-    pass
+    y = [4,2,3]
+    quick_sort(y)
+    print(y)
