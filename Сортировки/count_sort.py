9e627c0046291290c5b60089267c2c217a068f77
bakmaks <gitbakmax@yandex.ru>
2020-07-30 10:27:46 +0300

    count_sort()  в разработке

--- Сортировки/count_sort.py
+++ Сортировки/count_sort.py
@@ -1,3 +1,23 @@
-def count_sort(A):
-    """Сортировка подсчётом"""
-    pass
+import sys
+
+
+def count_sort(A, n, m):
+    """
+    Сортировка подсчётом.
+    Первая строка содержит число 1 <= n <= 10^4, вторая — n натуральных чисел,
+    не превышающих 10. Выведите упорядоченную по неубыванию последовательность этих чисел.
+    :param m - наибольшее число в списке
+    :param n - кол-во чисел
+    :param A - список чисел
+    """
+    a = [0] * m
+    print(a)
+
+
+if __name__ == '__main__':
+    reader = (map(int, line.split()) for line in sys.stdin)
+    n = list(next(reader))[0]
+    print(str(n))
+    x = list(next(reader))
+    print(x)
+    count_sort(x, n, 10)
