4582e526a3f89002a65c62874371e076db43fb54
bakmaks <gitbakmax@yandex.ru>
2020-07-31 09:05:53 +0300

    Реализована сортировка подсчётом

--- Сортировки/count_sort.py
+++ Сортировки/count_sort.py
@@ -1,23 +1,25 @@
 import sys
+from collections import Counter
 
 
-def count_sort(A, n, m):
+def count_sort(A, m):
     """
     Сортировка подсчётом.
     Первая строка содержит число 1 <= n <= 10^4, вторая — n натуральных чисел,
     не превышающих 10. Выведите упорядоченную по неубыванию последовательность этих чисел.
+
     :param m - наибольшее число в списке
-    :param n - кол-во чисел
     :param A - список чисел
     """
-    a = [0] * m
-    print(a)
+    a = Counter(A)  # Подсчёт кол-ва одинаковых значений
+    for key in range(m+1):    # Перебор значений по порядку. Известно, что значения  < 11
+        if key in a:          # Если значение есть в словаре
+            for _ in range(a[key]):
+                print(key, end=' ')   # Печать через пробел
 
 
 if __name__ == '__main__':
     reader = (map(int, line.split()) for line in sys.stdin)
-    n = list(next(reader))[0]
-    print(str(n))
     x = list(next(reader))
     print(x)
-    count_sort(x, n, 10)
+    count_sort(x, 10)
