b4d8d0f5e4325fcf8ce7cf53453f7f40a7684545
bakmaks <gitbakmax@yandex.ru>
2020-08-07 09:54:41 +0300

    --

--- Din_pogr/longest_increasing _subsequence.py
+++ Din_pogr/longest_increasing _subsequence.py
@@ -1,4 +1,4 @@
-def LIS(A: list):
+def LIS(A: list, n: int):
     """
     Дано целое число 1 ≤ n ≤ 10^3 и массив A[1…n] натуральных чисел, не превосходящих 2⋅10^9.
     Выведите максимальное 1 ≤ k ≤ n, для которого найдётся подпоследовательность 1 ≤ i1 < i2 < … < ik ≤ n
@@ -10,10 +10,20 @@ def LIS(A: list):
     ответ: 3
 
     :param A: Список
+    :param n: Длина списка
     """
+    ans = 0
+    d = [0] * n
+    for i in range(n):
+        d[i] = 1
+        for j in range(0, i-1):
+            if A[j] < A[i] and d[j] + 1 > d[i]:
+                d[i] = d[j] + 1
+    for i in range(n):
+        print(max(ans, d[i]), end=' ')
 
-    print(ans)
 
 if __name__ == '__main__':
+    n = int(input())
     x = list(map(int, input().split()))
-    LIS(x)
+    LIS(x, n)
